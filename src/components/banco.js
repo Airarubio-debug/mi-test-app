e9fd900f90c84e681ae0beaf1abe3c3e90b73f05
Maria <aira_rubio@hotmail.com>
2026-05-08 19:11:49 +0200

    eh

--- src/components/banco.js
+++ src/components/banco.js
@@ -1,25 +1,11 @@
-// src/data/tests.js
-// Importamos las preguntas de cada asignatura desde sus archivos individuales
-import { sostenibilidadQuestions } from './sostenibilidad'
-
-// Aquí irás añadiendo los nuevos archivos conforme los crees, por ejemplo:
-// import { redesQuestions } from './redes';
-// import { sistemasQuestions } from './sistemas';
+import { sostenibilidadQuestions } from './sostenibilidad'; // Asegúrate de que coincida con el nombre en GitHub
 
 export const initialTests = [
   {
     id: 1,
     name: 'Sostenibilidad (ASIR)',
     questions: sostenibilidadQuestions
-  },
-  /* Cuando tengas lista la siguiente asignatura, solo tienes que 
-  descomentar (quitar las barras) y rellenar este bloque:
-  {
-    id: 2,
-    name: 'Nombre de la Segunda Asignatura',
-    questions: nombreDeLaVariableImportada
-  }, 
-  */ 
+  }
 ];
 
 
